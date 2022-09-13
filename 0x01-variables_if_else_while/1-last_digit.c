8d3625187c3789c6a947b7df25add845c8c3421b
bek <bek.harvest@gmail.com>
2022-09-12 19:23:15 -0700

    C var

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 /**
  *  * main - main block
- *   * Description: Print statements based on the last digit of the random number.
- *    * Return: 0
- *     */
+ *  * Description: Print statement based on the last digit of the random number
+ *  * Return: 0
+*/
 int main(void)
 {
 		int n;
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  *  * main - main block
- *   * Description: Use `putchar` function to print the alphabet in lowercase.
- *    * Return: 0
- *     */
+ *  * Description: Use `putchar` function to print the alphabet in lowercase.
+ *  * Return: 0
+*/
 int main(void)
 {
 		char c = 'a';
